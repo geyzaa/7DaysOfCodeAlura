77caa5672bb894e2b51ac484abddcd10ec5e1de3
Geyza Oliveira <97538755+geyzaa@users.noreply.github.com>
2023-04-16 02:00:39 -0300

    salvando ideia do site

---
+++ Day2/day2.js
@@ -0,0 +1,9 @@
+/*const nome = prompt("Digite o seu nome:");
+const msg = `Olá ${nome}! Que bom que está aqui.`;
+alert(msg);
+const cidade = prompt("De onde você é?");
+const msg = `Você é de ${cidade}!`;
+alert(msg);
+const destino = prompt("Que país gostaria de conhecer?");
+const msg = `${destino} é um ótimo destino para viajar!`;
+alert(msg);
